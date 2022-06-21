48b61b21ab479ee0165059396d2d0b8fa723c456
truong-cot <engtruong1609@gmail.com>
2022-06-21 09:11:11 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <div>
-            <h1>Chào mừng bạn đến với TikTok</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Chào mừng bạn đến với TikTok</h1>
+    </div>
+  );
 }
